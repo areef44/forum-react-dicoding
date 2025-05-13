b99d4d28edd3b8899f9ac3873fdaeb0c27ba1f62
areef44 <areef.empat.empat@gmail.com>
2025-05-13 11:06:46 +0700

    fixing some testing script new

--- src/states/leaderboards/reducer.test.js
+++ src/states/leaderboards/reducer.test.js
@@ -8,45 +8,45 @@
  */
 
 import { describe, expect, it } from "vitest";
-import leaderboardsReducer from './reducer'
+import leaderboardsReducer from "./reducer";
 
 describe("leaderboards function", () => {
-    it('should return the initial state when given by unknown action', () => {
-        // arrange
-        const initialState = [];
-        const action = { type: "UNKNOWN"};
+  it("should return the initial state when given by unknown action", () => {
+    // arrange
+    const initialState = [];
+    const action = { type: "UNKNOWN" };
 
-        // action
-        const nextState = leaderboardsReducer(initialState, action)
+    // action
+    const nextState = leaderboardsReducer(initialState, action);
 
-        // assert
-        expect(nextState).toEqual(initialState)
-    })
+    // assert
+    expect(nextState).toEqual(initialState);
+  });
 
-    it('should return the leaderboards when given by RECEIVE_LEADERBOARDS action', () => {
-        // arrange
-        const initialState = [];
-        const action = { 
-            type: "RECEIVE_LEADERBOARDS",
-            payload: {
-                leaderboards: [
-                    {
-                        "user": {
-                            "id": "users-1",
-                            "name": "Muhammad Arif",
-                            "email": "example@example.com",
-                            "avatar": "https://generated-image-url.jpg"
-                        },
-                        "score": 10,
-                    },
-                ]
-            }
-        };
+  it("should return the leaderboards when given by RECEIVE_LEADERBOARDS action", () => {
+    // arrange
+    const initialState = [];
+    const action = {
+      type: "RECEIVE_LEADERBOARDS",
+      payload: {
+        leaderboards: [
+          {
+            user: {
+              id: "users-1",
+              name: "Muhammad Arif",
+              email: "example@example.com",
+              avatar: "https://generated-image-url.jpg",
+            },
+            score: 10,
+          },
+        ],
+      },
+    };
 
-        // action
-        const nextState = leaderboardsReducer(initialState, action)
+    // action
+    const nextState = leaderboardsReducer(initialState, action);
 
-        // assert
-        expect(nextState).toEqual(action.payload.leaderboards)
-    })
-})
+    // assert
+    expect(nextState).toEqual(action.payload.leaderboards);
+  });
+});
